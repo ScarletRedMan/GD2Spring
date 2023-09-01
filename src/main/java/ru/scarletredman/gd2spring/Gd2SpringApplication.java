122595aabec139da1b047c0b546361b6978c7546
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-09-01 12:56:13 +0700

    Initial commit

---
+++ src/main/java/ru/scarletredman/gd2spring/Gd2SpringApplication.java
@@ -0,0 +1,13 @@
+package ru.scarletredman.gd2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gd2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gd2SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/scarletredman/gd2spring/Gd2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.scarletredman.gd2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gd2SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
