2bd353e3c41a800172cd57ecec4524bd6a61a36b
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-09-05 10:21:59 +0700

    Added tests for gjp

---
+++ src/test/java/ru/scarletredman/gd2spring/GdPasswordTests.java
@@ -0,0 +1,21 @@
+package ru.scarletredman.gd2spring;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import ru.scarletredman.gd2spring.util.GdPasswordUtil;
+
+public class GdPasswordTests {
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", "hello", "sssssssssssssssssssssssssss", "39ur92jfdi2gh3-fsi834hsd", GdPasswordUtil.DEFAULT_KEY})
+    void testXor(String input) {
+        Assertions.assertEquals(input, GdPasswordUtil.xor(GdPasswordUtil.xor(input)));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", "hello", "sssssssssssssssssssssssssss", "39ur92jfdi2gh3-fsi834hsd", GdPasswordUtil.DEFAULT_KEY})
+    void testGjp(String sourceValue) {
+        Assertions.assertEquals(sourceValue, GdPasswordUtil.gjpDecode(GdPasswordUtil.gjpEncode(sourceValue)));
+    }
+}
