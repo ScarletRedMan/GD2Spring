c152bc1e8631818f97cc97093e8f06be7d884628
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-09-09 18:23:27 +0700

    Added empty account comment controller

---
+++ src/main/java/ru/scarletredman/gd2spring/controller/AccountCommentController.java
@@ -0,0 +1,36 @@
+package ru.scarletredman.gd2spring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.scarletredman.gd2spring.controller.annotation.GeometryDashAPI;
+import ru.scarletredman.gd2spring.security.annotation.AuthorizedOnly;
+
+@RestController
+@GeometryDashAPI
+@RequiredArgsConstructor
+public class AccountCommentController {
+
+    @AuthorizedOnly
+    @PostMapping("/getGJAccountComments20.php")
+    String accountComments(@RequestParam(name = "accountID") int accountId,
+                           @RequestParam(name = "page") int page,
+                           @RequestParam(name = "total") int total,
+                           @RequestParam(name = "secret") String secret) {
+        return "-1";
+    }
+
+    @AuthorizedOnly
+    @PostMapping("/uploadGJAccComment20.php")
+    String publishAccountComment(@RequestParam(name = "comment") String encodedText) {
+        return "-1";
+    }
+
+    @AuthorizedOnly
+    @PostMapping("/deleteGJAccComment20.php")
+    String deleteAccountComment(@RequestParam(name = "commentID") int commentId,
+                                @RequestParam(name = "cType") int wtfParam) {
+        return "-1";
+    }
+}
