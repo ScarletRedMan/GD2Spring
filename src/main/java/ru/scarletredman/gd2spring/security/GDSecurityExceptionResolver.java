291b1b3917b247b67c3f0c746b2e23348dff4e8a
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-09-12 22:05:49 +0700

    Added exception resolver for UserLoginError

---
+++ src/main/java/ru/scarletredman/gd2spring/security/GDSecurityExceptionResolver.java
@@ -0,0 +1,33 @@
+package ru.scarletredman.gd2spring.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
+import ru.scarletredman.gd2spring.service.exception.UserLoginError;
+
+import java.io.IOException;
+
+@Component
+public class GDSecurityExceptionResolver extends AbstractHandlerExceptionResolver {
+
+    @Override
+    protected ModelAndView doResolveException(@NonNull HttpServletRequest request,
+                                              @NonNull HttpServletResponse response,
+                                              Object handler,
+                                              @NonNull Exception ex) {
+
+        var error = ex.getCause();
+        if (error instanceof UserLoginError) {
+            try {
+                response.getWriter().write("-1");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return new ModelAndView();
+        }
+        return null;
+    }
+}
