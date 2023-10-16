282451a043124bf5d7d426b99005d28b84103917
ScarletRedMan <terentev.andrey.2002@gmail.com>
2023-10-16 13:07:36 +0700

    Implemented messages repository, service, controller (#4)

--- src/main/java/ru/scarletredman/gd2spring/controller/response/MessageResponse.java
+++ src/main/java/ru/scarletredman/gd2spring/controller/response/MessageResponse.java
@@ -1,31 +1,65 @@
 package ru.scarletredman.gd2spring.controller.response;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.nio.charset.StandardCharsets;
 import java.util.EnumMap;
 import java.util.Map;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.apache.commons.codec.binary.Base64;
 import ru.scarletredman.gd2spring.controller.response.json.ResponseSerializer;
+import ru.scarletredman.gd2spring.model.Message;
 import ru.scarletredman.gd2spring.util.JoinResponseUtil;
+import ru.scarletredman.gd2spring.util.TimeFormatUtil;
 
 @Getter
 @Setter
 @JsonSerialize(using = ResponseSerializer.class)
-public class MessageResponse implements ResponseSerializer.Response {
+public final class MessageResponse implements ResponseSerializer.Response {
 
     private final Map<Key, Object> elements = new EnumMap<>(Key.class);
+    private final boolean error;
 
-    public MessageResponse() {
-        init();
+    private MessageResponse() {
+        error = true;
     }
 
-    private void init() {
-        // todo
+    public MessageResponse(Message message, boolean isSender) {
+        init(message, isSender);
+        error = false;
+    }
+
+    public static MessageResponse error() {
+        return new MessageResponse();
+    }
+
+    private void init(Message message, boolean isSender) {
+        var target = isSender ? message.getReceiver() : message.getSender();
+        var targetId = target.getId();
+
+        elements.put(Key.MESSAGE_ID, message.getId());
+        elements.put(Key.TARGET_USER_ID1, targetId);
+        elements.put(Key.TARGET_USER_ID2, targetId);
+        setSubject(message.getSubject());
+        setText(message.getText());
+        elements.put(Key.TARGET_USERNAME, target.getUsername());
+        elements.put(Key.UPLOAD_TIME, TimeFormatUtil.formatBetween(message.getTime()));
+        elements.put(Key.IS_SENDER, isSender ? 1 : 0);
+        elements.put(Key.IS_NEW, message.isNew() ? 1 : 0);
+    }
+
+    public void setSubject(String subject) {
+        elements.put(Key.SUBJECT, Base64.encodeBase64String(subject.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public void setText(String message) {
+        elements.put(Key.BODY, Base64.encodeBase64String(message.getBytes(StandardCharsets.UTF_8)));
     }
 
     @Override
     public String getResponse() {
+        if (error) return "-1";
         return JoinResponseUtil.join(elements, ":");
     }
 
@@ -33,13 +67,14 @@ public class MessageResponse implements ResponseSerializer.Response {
     @RequiredArgsConstructor
     public enum Key implements JoinResponseUtil.Key {
         MESSAGE_ID("1"),
-        USER_ID1("2"),
-        USER_ID2("3"),
+        TARGET_USER_ID1("2"),
+        TARGET_USER_ID2("3"),
         SUBJECT("4"),
-        USERNAME("6"),
+        BODY("5"),
+        TARGET_USERNAME("6"),
         UPLOAD_TIME("7"),
         IS_NEW("8"),
-        GET_SENT("9"),
+        IS_SENDER("9"),
         ;
 
         private final String code;
---
+++ src/main/java/ru/scarletredman/gd2spring/service/MessageService.java
@@ -0,0 +1,23 @@
+package ru.scarletredman.gd2spring.service;
+
+import java.util.Collection;
+import java.util.Optional;
+import org.springframework.security.access.AccessDeniedException;
+import ru.scarletredman.gd2spring.model.Message;
+import ru.scarletredman.gd2spring.model.User;
+import ru.scarletredman.gd2spring.service.type.MessageListPage;
+
+public interface MessageService {
+
+    Optional<Message> getMessageById(long id);
+
+    MessageListPage getMessages(User user, boolean sent, int page, int limit);
+
+    Message sendMessage(User sender, User receiver, String subject, String text);
+
+    void deleteMessage(User user, Message message) throws AccessDeniedException;
+
+    void deleteMessagesById(User user, Collection<Long> messageIds) throws AccessDeniedException;
+
+    void deleteMessages(User user, Collection<Message> messages) throws AccessDeniedException;
+}
